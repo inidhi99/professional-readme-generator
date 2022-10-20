3ba327bd5d524a3a159c181bb820148739c02c01
nidhi <inidhi99@gmail.com>
2022-10-19 22:09:50 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+// fs is a Node standard library package for reading and writing files
+const fs = require("fs");
+// inquirer variable 
+const inquirer = require("inquirer");
+// connecting it to generateMarkdown.js
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        message: "Please enter a description of your project.",
+        name: "description",
+        validate: validateInput,
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('It is essential to link to your GitHub repo so users know where to find more of your work');
+                return false;
+            }
+        }
+    },
+    
+
+];
+
+
+//  /* Legacy way: with this.async */
+//  validate: function (input) {
+//     // Declare function as asynchronous, and save the done callback
+//     const done = this.async();
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+}
 
 // Function call to initialize app
 init();
